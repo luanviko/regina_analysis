32818c730be3a7a791db630689222865012988a4
Luan Koerich <luanviko@gmail.com>
2023-08-18 17:09:25 -0600

    Adding TOF analysis files

---
+++ TOF_analysis.py
@@ -0,0 +1,46 @@
+import numpy as np, progressbar
+from regina_analysis import *
+
+# Scale factors
+h_factor = 1./500.e6*1.e9
+v_factor = 1./16384.*2.*1.e3
+npz_file_path  = "../data/pulse_information-000435.npz"
+
+# Load time information
+print("Loading file...", end="\r")
+run_number = get_run_number(npz_file_path)
+count, baseline, timing, amplitude, event_number = load_information(npz_file_path)
+number_of_events = int(1.*len(count))
+print("Loading file... Done.")
+
+# Find TOF channels and keep pulses of higher amplitudes
+# timing = timing_in
+timing = [timing[i] for i in range(8,16)]
+amplitude = [amplitude[i] for i in range(8,16)]
+# timing = [timing[i][abs(amplitude[i]) > 40] for i in range(0,8)]
+
+# Start progress bar
+widgets=[f'TOF analysis: ', progressbar.Percentage(), progressbar.Bar('\u2587', '|', '|'), ' ', progressbar.Timer()]            
+bar = progressbar.ProgressBar(widgets=widgets, maxval=number_of_events).start()
+
+# Start analysis
+TOF_1 = np.array([], dtype="float")
+TOF_2 = np.array([], dtype="float")
+save_event_number = []
+skipped_events = np.array([], dtype="int")
+for i in range(0, number_of_events):
+    bar.update(i)
+    try:
+        TOF = generate_TOF_array(timing[i], amplitude[i], 2)
+        TOF_1 = np.append(TOF_1, [TOF[0]], axis=None)
+        TOF_2 = np.append(TOF_2, [TOF[1]], axis=None)
+        save_event_number = np.append(save_event_number, [event_number[i][0]], axis=None)
+    except IndexError:
+        skipped_event.append(i)
+
+if len(skipped_events) > 0:
+    write_warning(run_number, skipped_events)
+    
+np.savez_compressed(f"../data/TOF_{run_number}.npz", TOF_1=TOF_1, TOF_2=TOF_2, event_number=save_event_number)
+
+    
---
+++ regina_analysis.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def write_warning(run_number, skipped_events):
+    with open(f"./TOF-{run_number}.log") as log:
+        log.write("These events were skipped:\n")
+        for i in range(0, len(skipped_events):
+            log.write(f"{i}\n")
+
+def get_run_number(root_file_path):
+    # Split path of files named "root_run_XXXXXX.root" to find XXXXXX
+    # @params: _root_file_path_ containing the Linux/Mac path to the root file
+    # @return: string with XXXXXX
+    split1 = root_file_path.split("-")
+    run_number = split1[-1].replace(".npz","")
+    return run_number
+
+
+def load_event_information(event_number, npz_file_path):
+    data = np.load(npz_file_path, allow_pickle=True)
+    print("Looking for pulse information...")
+    entry = np.where(data["event_number"]==event_number)[0][0]
+    return data["count"][entry], data["baseline"][entry], data["CFD_timing"][entry], data["amplitude"][entry]
+
+
+def load_information(npz_file_path):
+    data = np.load(npz_file_path, allow_pickle=True)
+    return data["count"], data["baseline"], data["CFD_timing"], data["amplitude"], data["event_number"]
+
+
+def generate_TOF_matrix():
+    pass
+
+
+def generate_TOF_array(timing, amplitude, N):
+    sorted_amps = [np.argsort(amplitude[i]) for i in range(0,8)]
+    TOF_j = np.array([], dtype="float")
+    for j in range(0, N):
+        TOF0_j = np.average([timing[i][j] for i in range(0,4) if len(timing[i]) > j ])
+        TOF1_j = np.average([timing[i][j] for i in range(4,8) if len(timing[i]) > j ])
+        TOF_j  = np.append(TOF_j, [TOF1_j - TOF0_j], axis = None)
+    return TOF_j
+    
+    
